ad35e061a2a1abd4911b641c4a2e770d8e7a313b
krlcssiemens-source <krlcssiemens@gmail.com>
2025-12-19 10:22:15 +0900

    change py

--- test_hello.py
+++ test_hello.py
@@ -4,7 +4,7 @@ from hello import say_hello
 class TestHello(unittest.TestCase):
     def test_say_hello(self):
         # 결과가 "Hello Jenkins!"가 맞는지 확인
-        self.assertEqual(say_hello(), "Hello Jenkins!")
+        self.assertEqual(say_hello(), "Hello Jenkins! change")
 
 if __name__ == '__main__':
     unittest.main()
