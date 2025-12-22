81ee2ce591b617c73a18fec01ab5bb1366ba3195
krlcssiemens-source <krlcssiemens@gmail.com>
2025-12-22 16:19:59 +0900

    Update AppTest.java

--- src/test/java/testapp/junit/AppTest.java
+++ src/test/java/testapp/junit/AppTest.java
@@ -7,56 +7,56 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
+
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     int a = 1;
     int b = 1;
-	int c = 0;
-    public void testApp()
-    {
-        assertTrue( true );
+    int c = 0;
+
+    /** {@wi.implements POCG/POCG-1314} */
+    public void testApp() {
+        assertTrue(true);
     }
-    
-    public void testAdd()
-    {
-        assertTrue( a+b==2 );
+
+    /** {@wi.implements POCG/POCG-1315} */
+    public void testAdd() {
+        assertTrue(a + b == 2);
     }
-    
-    public void testSubtract()
-    {
-        assertTrue( a-b==0 );
+
+    /** {@wi.implements POCG/POCG-1316} */
+    public void testSubtract() {
+        assertTrue(a - b == 0);
     }
-    
-    public void testMultiply()
-    {
-        assertTrue( a*b==1 );
+
+    /** {@wi.implements POCG/POCG-1317} */
+    public void testMultiply() {
+        assertTrue(a * b == 1);
     }
-    
-    public void testDivide()
-    {
-        assertTrue( a/b==1 );
+
+    /** {@wi.implements POCG/POCG-1318} */
+    public void testDivide() {
+        assertTrue(a / b == 1);
     }
-    public void testFailure()
-    {
-    	assertTrue( a-c==11 );
+
+    /** * POCG-1319 등 추가 ID가 필요하다면 여기에 할당할 수 있습니다.
+     */
+    public void testFailure() {
+        assertTrue(a - c == 11);
     }
 }
