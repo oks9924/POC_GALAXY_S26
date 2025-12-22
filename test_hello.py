b8a80a84fe76d7bec99d168d0e2e47712a116c6e
krlcssiemens-source <krlcssiemens@gmail.com>
2025-12-22 13:58:05 +0900

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,4 @@
-# {@wi.implements POC_GALAXY_S26/TEST-1234}
+# {@wi.implements POC_GALAXY_S26/POCG-1311}
 # 위 주석이 Polarion과 이 소스 코드를 연결하는 핵심 태그입니다.
 def test_say_hello():
     print("Executing Test for TEST-1234")
